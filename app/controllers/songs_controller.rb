4c77409c4ce60d9405dd8045c5ffa1be47efb727
Sean Oughton <seanoughton@gmail.com>
2018-04-30 19:45:19 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,6 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    #@song.note_contents = (params[:song][:note_contents])
 
     if @song.save
       redirect_to @song
